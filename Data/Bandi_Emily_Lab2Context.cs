f28c8489d5e7ee79797030066375e84c6b76ec55
bandiemily <bandiemili0607@gmail.com>
2023-11-06 15:31:06 +0200

    Add project files.

---
+++ Data/Bandi_Emily_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bandi_Emily_Lab2.Models;
+
+namespace Bandi_Emily_Lab2.Data
+{
+    public class Bandi_Emily_Lab2Context : DbContext
+    {
+        public Bandi_Emily_Lab2Context (DbContextOptions<Bandi_Emily_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bandi_Emily_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Bandi_Emily_Lab2.Data;
+using Bandi_Emily_Lab2.Models;
+
+namespace Bandi_Emily_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Bandi_Emily_Lab2.Data.Bandi_Emily_Lab2Context _context;
+
+        public IndexModel(Bandi_Emily_Lab2.Data.Bandi_Emily_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
